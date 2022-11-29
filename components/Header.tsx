123f8b8991b8d4dbd62338441a37eb5b62bbf1fd
Julien Genestoux <julien.genestoux@gmail.com>
2022-11-29 15:31:09 -0500

    adding auth

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,32 @@
+import { useAuth } from "../hooks/useAuth";
+
 const Header = () => {
+  const { isAuthenticated, login, logout } = useAuth();
+
   return (
     <nav className="px-5 bg-gray-800">
       <div className="relative text-white flex h-16 items-center justify-end">
-        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-          Connect Wallet
-        </button>
+        <div>
+          {isAuthenticated ? (
+            <button
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => {
+                logout();
+              }}
+            >
+              Logout
+            </button>
+          ) : (
+            <button
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => {
+                login();
+              }}
+            >
+              Connect Wallet
+            </button>
+          )}
+        </div>
       </div>
     </nav>
   );
---
+++ hooks/useAuth.tsx
@@ -0,0 +1,82 @@
+import { useRouter } from "next/router";
+import { useEffect, useMemo, useCallback, useState } from "react";
+import { ethers } from "ethers";
+import useLocalStorageState from "use-local-storage-state";
+
+export function useAuth() {
+  const router = useRouter();
+  const code = router.query?.code?.toString();
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const [user, setUser] = useLocalStorageState("user", {
+    defaultValue: ""
+  });
+
+  const isAuthenticated = !!user;
+  const authenticate = useCallback(
+    async (_code: string) => {
+      const code = JSON.parse(Buffer.from(_code, "base64").toString());
+      const message = code.d;
+      const signature = code.s;
+      const user = ethers.utils.verifyMessage(message, signature);
+
+      setUser(user);
+
+      return {
+        message,
+        signature,
+        user
+      };
+    },
+    [setUser]
+  );
+
+  useEffect(() => {
+    if (!code) {
+      return;
+    }
+    const onCode = async () => {
+      setIsAuthenticating(true);
+      try {
+        // We just need to remove the code!
+        const url = new URL(window.location.toString());
+        url.searchParams.delete("code");
+        url.searchParams.delete("state");
+        router.replace(url.toString(), undefined, {
+          shallow: true
+        });
+        await authenticate(code);
+      } catch (error) {
+        console.error(error);
+      }
+      setIsAuthenticating(false);
+    };
+    onCode();
+  }, [router, code, authenticate]);
+
+  interface LoginParams {
+    [key: string]: string;
+  }
+
+  const login = (params: LoginParams = {}) => {
+    const url = new URL("https://app.unlock-protocol.com/checkout");
+    url.searchParams.set("client_id", window.location.host);
+    const redirectUri = new URL(window.location.href);
+    Object.keys(params).forEach(key => {
+      redirectUri.searchParams.set(key, params[key]);
+    });
+    url.searchParams.set("redirect_uri", redirectUri.toString());
+    window.location.href = url.toString();
+  };
+
+  const logout = () => {
+    setUser("");
+  };
+
+  return {
+    user,
+    login,
+    logout,
+    isAuthenticated,
+    isAuthenticating
+  };
+}
