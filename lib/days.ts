d98e76e85b641f5db4a5e27a0e0d9e3cb8d6ff28
Julien Genestoux <julien.genestoux@gmail.com>
2022-11-30 17:25:12 -0500

    adding the locks for each day

--- lib/days.ts
+++ lib/days.ts
@@ -4,6 +4,7 @@
 
 const days = [
   {
+    lock: "0x6B6CbaA6b44D5949A2a6cac24499aa13D6c1798D",
     title: "Welcome to the 2022 Unlock Protocol Advent Calendar!",
     description: `Every day, open a new door, claim a new NFT, and learn something new! You can open today’s door if you opened yesterday’s door. (No peeking ahead!) Best of all, some days will have special gifts for you from Unlock!`,
     link: "" /** Shall we put the link to NFT on OpenSea? */,
@@ -11,6 +12,7 @@ const days = [
     youtube: "",
   },
   {
+    lock: "0xff5446f83fFb3Ccd7D5f798C1F1C1D981E2ff2AE",
     title: "Year in Review",
     description: `Some days we’ll share great stuff from the the Unlock community’s amazing 2022 accomplishments and adventures. For example — did you know that all the tickets for Paris’s ETH.CC conference in June 2022 (over 1800 tickets!) were powered by Unlock? It’s true!`,
     link: "https://unlock-protocol.com/blog/ethcc5-2022-ticketing",
@@ -18,12 +20,75 @@ const days = [
     youtube: "",
   },
   {
-    title: "Would you like to play a game?",
+    lock: "0x27787E46a701CfFaaD8af059917CF0d626556568",
     description:
       "In 2022, BAFTA-award winning game designer Henry Hoffman integrated Unlock into Unity, enabling Unlock NFTs to be used for membership access, in-game items, and more in the Unity ecosystem — without having to write code. 🤯",
     link: "https://twitter.com/UnlockProtocol/status/1597632023362879488",
     image: "",
     youtube: "",
+  },
+  {
+    lock: "0x7608F73A1dFEb206A3A75aCDad6DC9FbA9Ba83D0",
+  },
+  {
+    lock: "0x3e36C285E11DE77e8257Fe7D49e80209C48b295E",
+  },
+  {
+    lock: "0x3Ba3470ffAB4D0bE96C75c5A11AA83DB7DC6501a",
+  },
+  {
+    lock: "0xd1bc4E5100024428E5573e0Cd7b1EB14e2c2aa73",
+  },
+  {
+    lock: "0x412024855fA62752805c96F1dbaD6B4dC0C9AD52",
+  },
+  {
+    lock: "0x2b7cd7B41f3937c09E4242828F08fCBBaf1043a5",
+  },
+  {
+    lock: "0xB3903433B3da787a01Aa30b73Ad1C108F632b297",
+  },
+  {
+    lock: "0x59c55EFD6faa9Dbecb3AdA3d219a898dd42A9Baf",
+  },
+  {
+    lock: "0x82b0728f060919c81F5fa0b74cF0889AC4227DA0",
+  },
+  {
+    lock: "0x3DFD892A1806c91663F2F145DcE9980DA92f186E",
+  },
+  {
+    lock: "0x07791a5C83fec114F4dEd574f62Aa9f78b3F3A73",
+  },
+  {
+    lock: "0x7F85E8cD1739986d9fd14118fA75c0F1d2365C1C",
+  },
+  {
+    lock: "0x4e50c69dcD6DBE9EDAED73e81643258C923d75f3",
+  },
+  {
+    lock: "0x0a051fD5673F8c317B53B0736676E89b1AF07797",
+  },
+  {
+    lock: "0x58D41c526A2D88aE9D08a546d327D3619511B278",
+  },
+  {
+    lock: "0x281522e8073bA6746874aa61bDF81DFa9DB833eC",
+  },
+  {
+    lock: "0x19b24fF3c0C124B91b672Dc8F8Ae077A96E2551C",
+  },
+  {
+    lock: "0x0634905430e4DB8CAaF63b0dd078EEFBF3FC1EdE",
+  },
+  {
+    lock: "0xE5D92b4F0953EeE7B250d2DFaeC955ad95161b22",
+  },
+  {
+    lock: "0xc9221Cf0004A05f04aB5dF82d58eb63D5307C040",
+  },
+  {
+    lock: "0xd8aD6E0C1aa8308e27F537A76032f50d2FE3e65D",
   },
 ];
 
