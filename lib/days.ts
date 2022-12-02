dce84e7298d2caed338d42daea6d73bb2ba9c9d8
Christopher Carfi <ccarfi@gmail.com>
2022-12-02 08:34:39 -0800

    Update days.ts

--- lib/days.ts
+++ lib/days.ts
@@ -4,6 +4,7 @@
 
 const days = [
   {
+    // Day 1
     lock: "0x6B6CbaA6b44D5949A2a6cac24499aa13D6c1798D",
     title: "🍾 Congrats! You have opened the first day of the advent calendar!",
     description: `Come back **every** day, open a new door, claim a new NFT, and learn something new! You can open today’s door if you opened yesterday’s door. (No peeking ahead!) Best of all, some days will have special gifts for you from Unlock! 
@@ -14,6 +15,7 @@ See you tomorrow!`,
     youtube: "",
   },
   {
+    // Day 2
     lock: "0xff5446f83fFb3Ccd7D5f798C1F1C1D981E2ff2AE",
     title: "Year in Review",
     description: `Some days we’ll share great stuff from the the Unlock community’s amazing 2022 accomplishments and adventures. For example — did you know that all the tickets for Paris’s ETH.CC conference in June 2022 (over 1800 tickets!) were powered by Unlock? It’s true!`,
@@ -22,7 +24,9 @@ See you tomorrow!`,
     youtube: "",
   },
   {
+    // Day 3
     lock: "0x27787E46a701CfFaaD8af059917CF0d626556568",
+    title: "Would you like to play a game?",
     description:
       "In 2022, BAFTA-award winning game designer Henry Hoffman integrated Unlock into Unity, enabling Unlock NFTs to be used for membership access, in-game items, and more in the Unity ecosystem — without having to write code. 🤯",
     link: "https://twitter.com/UnlockProtocol/status/1597632023362879488",
@@ -30,10 +34,22 @@ See you tomorrow!`,
     youtube: "",
   },
   {
+    // Day 4
     lock: "0x7608F73A1dFEb206A3A75aCDad6DC9FbA9Ba83D0",
+    title: "Get the flock out of here!",
+    description: "Flocker, built with Unlock, is a way your fans and followers can connect with you all across the web. We built it because you should be able to connect directly with them, and not have to rely on centralized platforms like the bird app which may go through some, ahem, changes.",
+    link: "https://flocker.app",
+    image: "",
+    youtube: "",
   },
   {
+    // Day 5
     lock: "0x3e36C285E11DE77e8257Fe7D49e80209C48b295E",
+    title: "Just another magic Monday",
+    description: "Hey! A little something-something for you today — we’re dropping a some USDC to you! We’ll automatically drop it into the wallet you used to get today’s NFT. Enjoy!",
+    link: "",
+    image: "",
+    youtube: "",
   },
   {
     lock: "0x3Ba3470ffAB4D0bE96C75c5A11AA83DB7DC6501a",
