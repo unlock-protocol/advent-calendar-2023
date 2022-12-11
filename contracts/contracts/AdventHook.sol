5395ee34b248d37f0d18b1d56398e45486f78a1f
Julien Genestoux <julien.genestoux@gmail.com>
2022-12-11 16:54:50 -0500

    day 12: donate 1 USDC to Bloom for every gate

--- contracts/contracts/AdventHook.sol
+++ contracts/contracts/AdventHook.sol
@@ -49,21 +49,20 @@ contract AdventHook {
   function onKeyPurchase(
       uint256, /* tokenId */
       address, /* from */
-      address recipient, /* recipient */
+      address, /* recipient */
       address, /* referrer */
       bytes calldata, /* data */
       uint256, /* minKeyPrice */
       uint256 /* pricePaid */
   ) external {
     uint lock = dayByLock[msg.sender];
-    if (lock == 5) {
+    if (lock == 12) {
       IERC20 usdc = IERC20(0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174);
       uint balance = usdc.balanceOf(address(this));
       if(balance >= 1000000) {
-        usdc.transfer(recipient, 1000000);
+        usdc.transfer(0x5219ffb88175588510e9752A1ecaA3cd217ca783, 1000000);
       }
     }
-
   }
 
 }
