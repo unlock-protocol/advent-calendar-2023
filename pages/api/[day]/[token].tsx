802d56205acc657dbcb932555f3cf5a71910b26d
Julien Genestoux <julien.genestoux@gmail.com>
2023-11-30 21:05:02 -0500

    fixing nft metadata

--- pages/api/[day]/[token].tsx
+++ pages/api/[day]/[token].tsx
@@ -2,21 +2,23 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { AppConfig } from '../../../lib/AppConfig'
  
 type ResponseData = {
-  message: string
+  description: string
+  external_url: string
+  image: string
+  name: string
+  attributes: any[]
+
 }
  
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  console.log(req.query)
-  // Ok so we have the day and the token
-  // We need to render the metadata!
   res.status(200).json({
     "description": AppConfig.description, 
     "external_url": AppConfig.siteUrl, 
     "image": `${AppConfig.siteUrl}/images/nft/${req.query.day}.png`, 
     "name": `Unlock Protocol Advent Calendar - Day ${req.query.day} - Token ${req.query.token}`,
-    "attributes": [  ]
+    "attributes": [    ]
   })
 }
