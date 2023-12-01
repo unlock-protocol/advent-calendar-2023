27722f480e7b9810250c23ec4e374d3440957fa1
Julien Genestoux <julien.genestoux@gmail.com>
2023-11-30 21:08:44 -0500

    updated URL

--- lib/AppConfig.ts
+++ lib/AppConfig.ts
@@ -3,7 +3,7 @@ export const AppConfig = {
   description: "Unlock Advent Calendar 2023",
   production: process.env.NODE_ENV === "production",
   environment: process.env.NODE_ENV!,
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL! || "http://localhost:3000",
+  siteUrl: process.env.NEXT_PUBLIC_VERCEL_URL! || "http://localhost:3000",
   unlockAppUrl: process.env.NEXT_PUBLIC_UNLOCK_APP_URL!,
   googleAnalyticsId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!,
   hotjarId: Number(process.env.NEXT_PUBLIC_HOTJAR_ID!),
