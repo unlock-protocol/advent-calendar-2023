267220230abb3fbded34f99a7cac7bc6d96f528f
Julien Genestoux <julien.genestoux@gmail.com>
2023-12-02 19:04:51 -0500

    ui improvements

--- lib/Provider.tsx
+++ lib/Provider.tsx
@@ -5,12 +5,13 @@ import { ReactNode } from "react";
 import {
   QueryClientProvider,
 } from '@tanstack/react-query'
+import { Toaster } from "react-hot-toast";
 
 import { queryClient } from "./reactQuery";
 import { DefaultSeo } from "next-seo";
 import { DEFAULT_SEO } from "./seo";
-import { useRouter } from "next/router";
 import { PrivyWagmiConnector } from "@privy-io/wagmi-connector";
+import * as RadixToolTip from "@radix-ui/react-tooltip";
 
 export const Provider = ({ children }: { children?: ReactNode }) => {
   const seo = { ...DEFAULT_SEO };
@@ -20,7 +21,17 @@ export const Provider = ({ children }: { children?: ReactNode }) => {
       <PrivyProvider appId="clpjz90qo00k2if0fl2coy0ns">
         <PrivyWagmiConnector wagmiChainsConfig={configureChainsConfig}>
           <DefaultSeo {...seo} />
+          <RadixToolTip.Provider delayDuration={100}>
           {children}
+          <Toaster
+              toastOptions={{
+                style: {
+                  background: "white",
+                  color: "black",
+                },
+              }}
+            />
+          </RadixToolTip.Provider>
         </PrivyWagmiConnector>
       </PrivyProvider>
     </QueryClientProvider>
