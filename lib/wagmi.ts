8d9421c41440bb7fa4f72cbab74af245d296c163
Julien Genestoux <julien.genestoux@gmail.com>
2023-12-15 16:58:48 -0500

    using unlock's provoder

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -13,7 +13,7 @@ const chains = Object.values(networks)
           http: [item?.provider],
         },
         public: {
-          http: [item?.publicProvider],
+          http: [item?.provider],
         },
       },
       name: item.name,
